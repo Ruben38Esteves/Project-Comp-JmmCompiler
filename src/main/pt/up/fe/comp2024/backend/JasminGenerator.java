2924ed1c1c4d9610c3d0422c91a0c521b49cac55
Miguel Dionísio <miguelmdionisio@gmail.com>
2024-05-24 22:14:16 +0100

    readded binary

--- src/main/pt/up/fe/comp2024/backend/JasminGenerator.java
+++ src/main/pt/up/fe/comp2024/backend/JasminGenerator.java
@@ -291,6 +291,13 @@ public class JasminGenerator {
             case LTE -> "if_icmple ";
             case GTE -> "if_icmpge ";
             case GTH -> "if_icmpgt ";
+            case NEQ -> "if_icmpne ";
+            case EQ -> "if_icmpeq ";
+            case AND -> "iand\n";
+            case OR -> "ior\n";
+            case NOT -> "iconst_1\nixor\n";
+            case NOTB -> "iconst_1\nixor\n";
+            
             default -> throw new NotImplementedException(binaryOp.getOperation().getOpType());
         };
 
